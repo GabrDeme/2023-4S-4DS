7faa571c579ec5c75730a72c6dcaa421fa88fdee
Gabriel Da Silva Demétrio <53144094889@info.local>
2024-08-06 08:12:20 -0300

    Exercícios III e IV finalizados

---
+++ tests-02-08/Exercise_IV/InventoryManagement.Test/InventoryUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InventoryManagement.Test
+{
+    public class InventaryUnitTest
+    {
+        public InventaryUnitTest()
+        {
+            Inventory.ResetInventory();
+        }
+
+        [Fact]
+        public void TestingInventoryManagementMethod()
+        {
+            var product = "Produto Teste";
+            var amount = 5;
+
+            Inventory.AddProduct(product, amount);
+
+            Assert.Equal(amount, Inventory.GetAmountOfProducts(product));
+        }
+
+        [Fact]
+        public void TestingExistingProductMethod()
+        {
+            var product = "Produto Teste";
+            var initialAmount = 5;
+            var AdditionalAmount = 3;
+
+            Inventory.AddProduct(product, initialAmount);
+            Inventory.AddProduct(product, AdditionalAmount);
+
+            Assert.Equal(initialAmount + AdditionalAmount, Inventory.GetAmountOfProducts(product));
+        }
+
+        [Fact]
+        public void TestingNonexistentProductMethod()
+        {
+            var product = "Produto Inexistente";
+
+            var amount = Inventory.GetAmountOfProducts(product);
+
+            Assert.Equal(0, amount);
+        }
+    }
+}
---
+++ tests-02-08/Exercise_IV/InventoryManagement/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public static class Inventory
+    {
+        private static Dictionary<string, int> products = new Dictionary<string, int>();
+
+        public static void AddProduct(string name, int amount)
+        {
+            if (products.ContainsKey(name))
+            {
+                products[name] += amount;
+            }
+            else
+            {
+                products[name] = amount;
+            }
+        }
+
+        public static int GetAmountOfProducts(string name)
+        {
+            return products.ContainsKey(name) ? products[name] : 0;
+        }
+
+        public static void ResetInventory()
+        {
+            products.Clear();
+        }
+    }
+}
