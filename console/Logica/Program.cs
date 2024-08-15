6812d6c18eac7fdb35a5ce60ef08efb345deb312
Gabriel Da Silva Demétrio <53144094889@info.local>
2024-08-15 10:24:13 -0300

    Exercício V finalizado

--- console/Logica/Program.cs
+++ console/Logica/Program.cs
@@ -2,28 +2,28 @@
 // Escreva um programa que peça ao usuário para digitar um número inteiro e informe se o
 // número é par ou ímpar. Utilize uma estrutura condicional if/else para realizar o teste.
 
-// Console.WriteLine("Tell me a number");
+Console.WriteLine("Tell me a number");
 
-// if (int.TryParse(Console.ReadLine(), out int number))
-// {
-//     bool choosedNumber(int num)
-//     {
-//         return num % 2 == 0;
-//     }
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    bool choosedNumber(int num)
+    {
+        return num % 2 == 0;
+    }
 
-//     if (choosedNumber(number))
-//     {
-//         Console.WriteLine("The number is even!");
-//     }
-//     else
-//     {
-//         Console.WriteLine("The number is odd!");
-//     }
-// }
-// else
-// {
-//     Console.WriteLine("Please enter a valid number.");
-// }
+    if (choosedNumber(number))
+    {
+        Console.WriteLine("The number is even!");
+    }
+    else
+    {
+        Console.WriteLine("The number is odd!");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number.");
+}
 
 // Exercício II
 // Crie um programa que permita que o usuário cadastre 5 nomes em um vetor. Após o
@@ -31,63 +31,55 @@
 // Utilize um laço for para o cadastro e um método de ordenação de sua preferência (por
 // exemplo, bubble sort) para ordenar os nomes.
 
-// string[] names = new string[5];
+string[] names = new string[5];
 
-//         // Cadastro dos nomes
-//         for (int i = 0; i < names.Length; i++)
-//         {
-//             Console.Write($"Type a name {i + 1}: ");
-//             names[i] = Console.ReadLine();
-//         }
-
-//         // Ordenação dos nomes usando Bubble Sort
-//         for (int i = 0; i < names.Length - 1; i++)
-//         {
-//             for (int j = 0; j < names.Length - i - 1; j++)
-//             {
-//                 if (string.Compare(names[j], names[j + 1]) > 0)
-//                 {
-//                     // Troca os nomes
-//                     string temp = names[j];
-//                     names[j] = names[j + 1];
-//                     names[j + 1] = temp;
-//                 }
-//             }
-//         }
-
-//         // Exibe os nomes em ordem alfabética
-//         Console.WriteLine("\nNames in the alphabetical order:");
-//         foreach (var name in names)
-//         {
-//             Console.WriteLine(name);
-//         }
+// Cadastro dos nomes
+for (int i = 0; i < names.Length; i++)
+{
+    Console.Write($"Type a name {i + 1}: ");
+    names[i] = Console.ReadLine();
+}
+// Ordenação dos nomes usando Bubble Sort
+for (int i = 0; i < names.Length - 1; i++)
+{
+    for (int j = 0; j < names.Length - i - 1; j++)
+    {
+        if (string.Compare(names[j], names[j + 1]) > 0)
+        {
+            // Troca os nomes
+            string temp = names[j];
+            names[j] = names[j + 1];
+            names[j + 1] = temp;
+        }
+    }
+}
+// Exibe os nomes em ordem alfabética
+Console.WriteLine("\nNames in the alphabetical order:");
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
 
 // Exercício III
 // Crie um programa que calcule a soma dos números pares de um vetor de 10 elementos.
 // Utilize um laço for para percorrer o vetor e uma estrutura condicional if para identificar
 // os números pares.
 
-// int[] vetor = new int[10];
-//         int soma = 0;
-
-//         // Preencher o vetor (pode ser com valores fixos ou entrada do usuário)
-//         for (int i = 0; i < vetor.Length; i++)
-//         {
-//             Console.Write($"Type {i + 1}º element: ");
-//             vetor[i] = int.Parse(Console.ReadLine());
-//         }
-
-//         // Calcular a soma dos números pares
-//         for (int i = 0; i < vetor.Length; i++)
-//         {
-//             if (vetor[i] % 2 == 0)
-//             {
-//                 soma += vetor[i];
-//             }
-//         }
-
-//         // Exibir o resultado
-//         Console.WriteLine($"The sum of the even numbers is: {soma}");
+int[] vetor = new int[10];
+int soma = 0;
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write($"Type {i + 1}º element: ");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < vetor.Length; i++)
+{
+    if (vetor[i] % 2 == 0)
+    {
+        soma += vetor[i];
+    }
+}
+Console.WriteLine($"The sum of the even numbers is: {soma}");
 
 // Exercício IV
 // Crie uma função que recebe um número como parâmetro e retorna a tabuada desse
@@ -101,4 +93,31 @@ int[] table = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for(int n = 1; n < table.Length; n++)
 {
     Console.WriteLine($"{number} x {n} = {number * n}");
+}
+
+// Exercício V
+// Crie um programa que peça ao usuário para digitar um texto e conte quantas vezes cada
+// letra do alfabeto aparece no texto.
+
+Console.WriteLine("Type a text:");
+string input = Console.ReadLine().ToLower();
+Dictionary<char, int> letterCount = new Dictionary<char, int>();
+foreach (char c in input)
+{
+    if (char.IsLetter(c))
+    {
+        if (letterCount.ContainsKey(c))
+        {
+            letterCount[c]++;
+        }
+        else
+        {
+            letterCount[c] = 1;
+        }
+    }
+}
+Console.WriteLine("\nCounting each letter:");
+foreach (var item in letterCount)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
 }
